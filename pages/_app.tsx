b987176adf10616c08dd7da063f34c7be38aba4b
Niihara Megumu <nhrmgm1223@gmail.com>
2022-06-21 22:32:14 +0900

    update: Session Providor.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
+import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
+import AuthWrapper from '../components/AuthWrapper'
+
+import Header from '../components/Header'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SessionProvider session={pageProps.session}>
+      <AuthWrapper>
+        <Header />
+        <Component {...pageProps} />
+      </AuthWrapper>
+    </SessionProvider>
+  )
 }
 
 export default MyApp
